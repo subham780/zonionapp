ddf55d1731a2f556c3c0092ad9da3079a60075d1
Akhil Shukla <36435276+akhs722@users.noreply.github.com>
2020-07-15 16:30:53 +0530

    Update constants.dart

--- lib/Utilities/constants.dart
+++ lib/Utilities/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final kGoogleApiKey = "AIzaSyDD6dMxH31dAVoCPC8Fi";
+final kGoogleApiKey = "VoCPC8Fi";
 const red = Colors.red;
 const white = Colors.white;
 const black = Colors.black;
